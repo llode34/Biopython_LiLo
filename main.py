8dbadc8919182a6de095819176a52ca941efb39b
liliana.audin <lgarcia@ensc.fr>
2016-11-17 18:17:21 +0100

    modify main

--- main.py
+++ main.py
@@ -1,12 +1,24 @@
 #!/usr/bin/env python
 
-from SeqRecord import *
+#import all qui est dans le fichier seqrecord
+from seqrecord import *
 from seq import *
+if __name__== "__main__":
+    filename="NC_005816.fna"
+    fasta_record=my_read_function(filename,"fasta")
+    print get_GC_content(fasta_record.seq)
+    gb_filename="NC_005816.gb"
+    gb_record=my_read_function(gb_filename,"genbank")
+    for feature in gb_record.features:
+        print feature.type
+        print feature.location
 
-if (__name__ == '__main__'):
-	fasta_filename = "NC_005816.fna"
-	record=my_read_function(fasta_filename, "fasta")
-	print get_GC_content(record.seq)
-	gb_filename="NC_005816.gb"
-	genbank_record=my_read_function(gb_filename, "genbank")
-	print genbank_record.features
+
+    
+
+#GC est egal au %de GC dans une sequence donne
+
+
+
+
+
